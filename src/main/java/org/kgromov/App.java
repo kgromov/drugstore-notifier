8eedab1fda9d131350e606b19f72b4383f1f269a
konstantin <konst.gromov@gmail.com>
2026-05-16 01:17:43 +0300

    Use jobrunr as fluent simple scheduler

--- src/main/java/org/kgromov/App.java
+++ src/main/java/org/kgromov/App.java
@@ -1,26 +1,18 @@
 package org.kgromov;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.TemporalAdjusters;
-import java.util.GregorianCalendar;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.scheduling.cron.Cron;
+import org.jobrunr.storage.InMemoryStorageProvider;
 
 public class App {
     static void main(String[] args) {
         var messageSender = new TelegramMessageSender();
-        var startOfNextMonth = LocalDate.now().with(TemporalAdjusters.firstDayOfNextMonth()).atStartOfDay();
-        var calendar = GregorianCalendar.from(ZonedDateTime.of(startOfNextMonth, ZoneId.systemDefault()));
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                messageSender.notifyRecipientsOnExpired();
-            }
-        }, calendar.getTime(), TimeUnit.DAYS.toMillis(30));
-
+        var jobScheduler = JobRunr.configure()
+                .useStorageProvider(new InMemoryStorageProvider())
+                .useBackgroundJobServer()
+                .useDashboard()
+                .initialize()
+                .getJobScheduler();
+        jobScheduler.scheduleRecurrently(Cron.monthly(1), messageSender::notifyRecipientsOnExpired);
     }
 }
